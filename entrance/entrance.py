140640e59d53bf40c546daeb9145c58733ab67c4
guowenlong <guowenlong@supremid.com>
2020-05-12 11:40:24 +0800

    entrance

---
+++ entrance/entrance.py
@@ -0,0 +1,17 @@
+from assist.assist import getModule
+from registry.loaderRegistry import getLoader
+from registry.entranceRegister import getEntrance
+from registry.optimizerRegistry import getOptimizerCreator
+from config import config
+
+
+def main():
+    loader = getLoader(config.phase)
+    device, model = getModule()
+    optimizer = getOptimizerCreator(config.optimizer)(model)
+    entrance = getEntrance(config.phase)
+    entrance(device, model, loader, optimizer)
+
+
+if __name__ == '__main__':
+    main()
